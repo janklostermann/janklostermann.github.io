019abd441ee27667b6fb75ea80d6c8d432459030
janklostermann <janklostermann@users.noreply.github.com>
2025-07-13 20:29:44 +0000

    Deploying to gh-pages from @ janklostermann/janklostermann.github.io@420e9ebaf1ccb1bb08edc51461247e047cb98a29 🚀

--- assets/js/better404/better404.js
+++ assets/js/better404/better404.js
@@ -2,11 +2,11 @@
 // Get root
 
 // Vars
-let homeUrl = "https://dieghernan.github.io/";
+let homeUrl = "https://janklostermann.github.io/";
 // with no trailing '/'
 homeUrl = homeUrl.replace(/\/+$/, "");
 let currentUrl = window.location.href;
-let sitemap = "https://dieghernan.github.io/sitemap.xml"
+let sitemap = "https://janklostermann.github.io/sitemap.xml"
 // Based in https://ben.balter.com/2022/06/30/helpful-404s-for-jekyll-and-github-pages/
 // A: Function definition...
 // Own version of closest from 'fastest-levenshtein')
